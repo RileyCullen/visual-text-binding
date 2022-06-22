3845d7240e34d99f5f558dec303a71fb238611f3
Riley Cullen <cullenriley@seattleu.edu>
2022-06-21 21:48:01 -1000

    getElementByID added

--- types/Managers/Manager.d.ts
+++ types/Managers/Manager.d.ts
@@ -5,6 +5,11 @@ export type TDataManagerElement = {
     value: Dataset
 }
 
-export interface IManager<T> {
+export interface IManager<T, V> {
     addElement: (name: string, data: T) => string;
+
+    /**
+     *  Method call returns an empty T object if uid is not found within manager.
+     */
+    getElementByID: (uid: string) => V;
 }
