475b4e46dab88ac828ebd4ef1df6211f4720f19b
Riley Cullen <cullenriley@seattleu.edu>
2022-06-21 21:46:49 -1000

    addDataset method added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
+import { TDataset } from "../types";
+import DataManager from "./Managers/DataManager";
+
 class VisualizationManager {
-    
+    #dataManager: DataManager;
+
+    constructor() {
+        this.#dataManager = new DataManager();
+    }
+
+    addDataset(name: string, data: TDataset): string {
+        return this.#dataManager.addElement(name, data);
+    }
 }
 
 export default VisualizationManager;
