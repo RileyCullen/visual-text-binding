412eb7a413a2712ff481d69ba24d9cb3b4e3f0ec
Riley Cullen <cullenriley@seattleu.edu>
2022-06-21 21:33:05 -1000

    IManager and TDataManagerElement exports added

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,6 @@
 export { TDataEntry } from './Dataset/DataEntry';
 export { TDataset } from './Dataset/Dataset';
 export { TDataElement } from './Dataset/Dataset';
+
+export { IManager } from './Managers/Manager';
+export { TDataManagerElement } from './Managers/Manager';
