701a1897f78e154a06e2c7cd5a04218c261d2462
Riley Cullen <cullenriley@seattleu.edu>
2022-06-23 21:46:03 -1000

    Support for visualizing dataset updated

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,42 @@
 import { TDataset } from "../types";
 import Dataset from "./Dataset/Dataset";
+import generateUID from "./Helpers/UID";
 import DataManager from "./Managers/DataManager";
-import VisualContainer from "./VisualContainer/VisualContainer";
+import createContainer from "./Helpers/VisualContainer";
+import DatasetVisualizer from "./Dataset/DatasetVisualizer";
 
 class VisualizationManager {
     #dataManager: DataManager;
+    #container: HTMLElement | null;
 
     constructor() {
         this.#dataManager = new DataManager();
-
-        // If custom elements not defined, define them.
-        // if (customElements.get('vis-container') === undefined) {
-        //     customElements.define('vis-container', VisualContainer);
-        // }
+        this.#container = null;
     }
 
     addDataset(name: string, data: TDataset): string {
         return this.#dataManager.addElement(name, data);
     }
 
-    getDataset(uid: string): Dataset {
+    getDataset(uid: string): Dataset | null{
         return this.#dataManager.getElementByID(uid);
+    }
+
+    visualizeDataset(uid: string): void {
+        // find dataset
+        let dataset = this.#dataManager.getElementByID(uid);
+        if (dataset == null) return;
+        // create DataVisualizer object
+        let visualizer = new DatasetVisualizer(dataset.getSubset());
+        // check if container exists
+        this.#container?.appendChild(visualizer.visualize());
+    }
+
+    createVisualContainer(width: number, height: number, addToID: string): void {
+        if (this.#container === null) {
+            this.#container = createContainer(generateUID(), width, height);
+            document.getElementById(addToID)?.appendChild(this.#container);
+        }
     }
 }
 
