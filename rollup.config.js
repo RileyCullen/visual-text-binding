43b3ad7af81acfb14eee0997c3fff28f3013492c
Riley Cullen <cullenriley@seattleu.edu>
2022-06-23 15:55:02 -1000

    Support for lodash added. Onwarn also added

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,26 @@
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { optimizeLodashImports } from '@optimize-lodash/rollup-plugin';
+
 export default {
     input: 'dist/index.js',
-    output: [
-        {
-            file: 'index.js',
-            name: 'index',
-            format: 'umd',
-            sourcemap: false,
-            freeze: false,
-        }
-    ]
+    external: ['lodash'],
+    onwarn: function(warning) {
+        if (warning.code === 'THIS_IS_UNDEFINED') { return; }
+        console.warn(warning.message);
+    },
+    output: {
+        file: 'VisualizationManager.js',
+        name: 'VisualizationManager',
+        format: 'umd',
+        sourcemap: false,
+        freeze: false,
+    },
+    plugins: [
+        commonjs({
+            include: 'node_modules/**'
+        }),
+        nodeResolve(),
+        optimizeLodashImports(),
+    ],
 };
