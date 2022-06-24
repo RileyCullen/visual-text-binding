7f2846133c1d403478cdc13df903ff2c88aaf58a
Riley Cullen <cullenriley@seattleu.edu>
2022-06-23 20:46:46 -1000

    Custom element vis-container added to custom registry

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import { TDataset } from "../types";
 import Dataset from "./Dataset/Dataset";
 import DataManager from "./Managers/DataManager";
+import VisualContainer from "./VisualContainer/VisualContainer";
 
 class VisualizationManager {
     #dataManager: DataManager;
 
     constructor() {
         this.#dataManager = new DataManager();
+
+        // If custom elements not defined, define them.
+        if (customElements.get('vis-container') === undefined) {
+            customElements.define('vis-container', VisualContainer);
+        }
     }
 
     addDataset(name: string, data: TDataset): string {
