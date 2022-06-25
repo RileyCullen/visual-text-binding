86e922731bef986060c9587843b52deee7648921
Riley Cullen <cullenriley@seattleu.edu>
2022-06-24 19:30:55 -1000

    Types for observer added

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,21 @@
+import Dataset from '../src/Dataset/Dataset';
+import { TDataset } from './Dataset/Dataset';
+
 export { TDataEntry } from './Dataset/DataEntry';
 export { TDataset } from './Dataset/Dataset';
+export { TTuple } from './Dataset/Dataset';
 export { TDataElement } from './Dataset/Dataset';
 
 export { IManager } from './Managers/Manager';
 export { TDataManagerElement } from './Managers/Manager';
+
+export interface IVisualElem {
+    update: (data: TDataset) => void
+}
+
+export type TObserverBindings = {
+    [uid: string]: {
+        dataset: Dataset,
+        dependencies: Array<IVisualElem>,
+    }
+};
